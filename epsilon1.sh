fac2f5397ad85e09ee354f4f0f5dc6219b768274
Sai Charitha Akula <charithaakula1996@gmail.com>
2023-03-02 13:40:00 -0500

    training scripts

---
+++ epsilon1.sh
@@ -0,0 +1,200 @@
+sbatch evalattacks10.sh deit3_small_224 /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_small_224 /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_small_224 /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_small_224 /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_small_224 /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_medium_224 /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_medium_224 /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_medium_224 /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_medium_224 /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_medium_224 /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_base_224 /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_base_224 /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_base_224 /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_base_224 /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_base_224 /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_large_224 /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_large_224 /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_large_224 /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_large_224 /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_large_224 /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_huge_224_14 /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_huge_224_14 /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_huge_224_14 /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_huge_224_14 /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit3_huge_224_14 /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beit_base_224 /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beit_base_224 /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beit_base_224 /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beit_base_224 /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beit_base_224 /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beit_large_224 /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beit_large_224 /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beit_large_224 /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beit_large_224 /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beit_large_224 /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beitv2_base_224 /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beitv2_base_224 /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beitv2_base_224 /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beitv2_base_224 /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beitv2_base_224 /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beitv2_large_224 /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beitv2_large_224 /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beitv2_large_224 /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beitv2_large_224 /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh beitv2_large_224 /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_base /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_base /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_base /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_base /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_base /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_huge /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_huge /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_huge /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_huge /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_huge /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_large /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_large /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_large /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_large /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh conv2_large /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh vit224 /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh vit224 /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh vit224 /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh vit224 /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh vit224 /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh resnet50 /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh resnet50 /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh resnet50 /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh resnet50 /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh resnet50 /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh resnet101d /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh resnet101d /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh resnet101d /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh resnet101d /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh resnet101d /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh wide-resnet /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh wide-resnet /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh wide-resnet /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh wide-resnet /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh wide-resnet /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit224_distill /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit224_distill /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit224_distill /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit224_distill /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh deit224_distill /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224 /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224 /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224 /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224 /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224 /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224base /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224base /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224base /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224base /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224base /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224large /imagenet/ 32 1 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224large /imagenet/ 32 2 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224large /imagenet/ 32 5 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224large /imagenet/ 32 10 10000 0.1 16 0 2/255
+sleep 2
+sbatch evalattacks10.sh swin224large /imagenet/ 32 20 10000 0.1 16 0 2/255
+sleep 2
